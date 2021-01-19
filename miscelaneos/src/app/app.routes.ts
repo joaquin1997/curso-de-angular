83e917ebcbe6c3d9fc90e8da93397b6a74e453bd
joaquin <joaquin19972009@hotmail.com>
2021-01-19 22:19:16 +0100

    9 152

--- miscelaneos/src/app/app.routes.ts
+++ miscelaneos/src/app/app.routes.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
+import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
+import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
+import { USUARIO_ROUTES } from './components/usuario/usuario.routes';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'usuario', component: UsuarioComponent },
+    { 
+        path: 'usuario/:id',
+        component: UsuarioComponent,
+        children: USUARIO_ROUTES
+    },
     { path: '**', pathMatch:'full', redirectTo: 'home' }
 ];
 
