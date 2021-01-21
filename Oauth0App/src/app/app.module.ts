e7d053de02eb7c36fdb8a82a5e6a6ddc77dd3134
Joaquin Garcia <jgarciagonzalez@deloitte.es>
2021-01-21 09:10:32 +0100

    añadimos auth service, cambios de compilacion

--- Oauth0App/src/app/app.module.ts
+++ Oauth0App/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NabvarComponent } from './components/nabvar/nabvar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { PreciosComponent } from './components/precios/precios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NabvarComponent,
+    HomeComponent,
+    ProtegidaComponent,
+    PreciosComponent
   ],
   imports: [
     BrowserModule,
